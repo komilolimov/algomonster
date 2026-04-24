a5edf1e66327008ba2e2523d99f25b5063df020c
Komil Olimov <k.olimov@uzinfocom.uz>
2026-04-24 12:34:11 +0500

    Fix code formatting for CI (Black)

--- src/binary_search/basic.py
+++ src/binary_search/basic.py
@@ -1,5 +1,6 @@
 from typing import List
 
+
 def binary_search(arr: List[int], target: int) -> int:
     """
     Находит индекс целевого элемента в отсортированном массиве.
@@ -10,21 +11,21 @@ def binary_search(arr: List[int], target: int) -> int:
 
     Returns:
         int: Индекс элемента, если он найден. В противном случае возвращает -1.
-        
+
     Time Complexity: O(log n), где n - длина массива.
     Space Complexity: O(1), так как используется константное количество дополнительной памяти.
     """
     left, right = 0, len(arr) - 1
-    
+
     while left <= right:
         # Избегаем потенциального переполнения типа integer (важно для понимания CS)
-        mid = left + (right - left) // 2  
-        
+        mid = left + (right - left) // 2
+
         if arr[mid] == target:
             return mid
         elif arr[mid] < target:
             left = mid + 1
         else:
             right = mid - 1
-            
+
     return -1
