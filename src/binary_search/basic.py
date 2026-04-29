8a5531fbc3606b559a5ab17058e8fa9c2fbf7d05
Komil Olimov <k.olimov@uzinfocom.uz>
2026-04-29 18:15:23 +0500

    Add binary search solution for boolean array

--- src/binary_search/basic.py
+++ src/binary_search/basic.py
@@ -5,7 +5,6 @@ def binary_search(arr: List[int], target: int) -> int:
     left, right = 0, len(arr) - 1
 
     while left <= right:
-
         mid = left + (right - left) // 2
 
         if arr[mid] == target:
--- src/binary_search/first_true.py
+++ src/binary_search/first_true.py
@@ -2,12 +2,12 @@ def find_boundary(arr: list[bool]) -> int:
     left, right = 0, len(arr) - 1
     boundary_index = -1
 
-    while left <= right: 
+    while left <= right:
         mid = left + (right - left) // 2
         if arr[mid]:
             boundary_index = mid
             right = mid - 1
         else:
             left = mid + 1
-            
-    return boundary_index
+
+    return boundary_index
