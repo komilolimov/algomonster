5576adb4647b711323c023fb1437ec9884969525
Komil Olimov <k.olimov@uzinfocom.uz>
2026-06-15 12:31:38 +0500

    find_min_rotatred

---
+++ src/binary_search/find_min_rotated.py
@@ -0,0 +1,28 @@
+from typing import List
+
+# A sorted array of unique integers was rotated at an unknown pivot. For example, [10, 20, 30, 40, 50] becomes [30, 40, 50, 10, 20]. Find the index of the minimum element in this array.
+
+# Input: [30, 40, 50, 10, 20]
+
+# Output: 3
+
+# Explanation: The smallest element is 10, and its index is 3.
+
+# Input: [3, 5, 7, 11, 13, 17, 19, 2]
+
+# Output: 7
+
+# Explanation: The smallest element is 2, and its index is 7.
+
+def find_min_rotated(arr: list[int]) -> int:
+    left, right = 0, len(arr) - 1
+    boundary_index = -1
+    anchor = arr[-1]
+    while left <= right:
+        mid = left + (right - left) // 2
+        if arr[mid] <= anchor:
+            boundary_index = mid
+            right = mid - 1
+        else:
+            left = mid + 1
+    return boundary_index
