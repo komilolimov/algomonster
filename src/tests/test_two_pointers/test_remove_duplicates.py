4097507bfa1c6a9fa98de7f56efbc3860549cbb5
Komil Olimov <k.olimov@uzinfocom.uz>
2026-06-23 16:46:25 +0500

    style: format code with ruff

--- src/tests/test_two_pointers/test_remove_duplicates.py
+++ src/tests/test_two_pointers/test_remove_duplicates.py
@@ -1,15 +1,16 @@
 import pytest
 from src.two_pointers.remove_duplicates import remove_duplicates
 
+
 @pytest.mark.parametrize(
-  "arr, expected",
-  [
-    ([0, 0, 1, 1, 1, 2, 2], 3),
-    ([0,0,1,1,1,2,2], 3),
-    ([1,2,3,4,5], 5),
-    ([1,1,1,1,1], 1),
-    ([1], 1)
-  ]
+    "arr, expected",
+    [
+        ([0, 0, 1, 1, 1, 2, 2], 3),
+        ([0, 0, 1, 1, 1, 2, 2], 3),
+        ([1, 2, 3, 4, 5], 5),
+        ([1, 1, 1, 1, 1], 1),
+        ([1], 1),
+    ],
 )
 def test_remove_duplicates(arr: list[int], expected: int) -> None:
     assert remove_duplicates(arr) == expected
