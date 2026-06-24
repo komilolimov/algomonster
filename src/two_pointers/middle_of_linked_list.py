30d314c8cd801be1dd35ed5260386ef83874394f
Komil Olimov <k.olimov@uzinfocom.uz>
2026-06-24 16:37:50 +0500

    Fix syntax error in middle of linked list test

---
+++ src/two_pointers/middle_of_linked_list.py
@@ -0,0 +1,20 @@
+# Middle of a Linked List
+# Find the middle node of a linked list.
+
+# Input: 0 1 2 3 4
+
+# Output: 2
+
+# If the number of nodes is even, then return the second middle node.
+
+# Input: 0 1 2 3 4 5
+
+# Output: 3
+
+
+def find_middle_of_linked_list(head):
+    slow = fast = head
+    while fast and fast.next:
+      fast = fast.next.next
+      slow = slow.next
+    return slow.value
