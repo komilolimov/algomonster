2c7d9788f6e7760abcc63c4f271e6d98efd7a962
Komil S. Olimov <komil.olimov@uic.local>
2026-07-14 11:53:50 +0500

    style: format index_of_first_occurrence_in_string.py with ruff

--- src/two_pointers/index_of_first_occurrence_in_string.py
+++ src/two_pointers/index_of_first_occurrence_in_string.py
@@ -6,7 +6,6 @@
 # Companies
 # Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
 
- 
 
 # Example 1:
 
@@ -32,7 +31,6 @@
 
 def strStr(haystack: str, needle: str) -> int:
     for i in range(len(haystack) - len(needle) + 1):
-        if haystack[i:i+len(needle)] == needle:
+        if haystack[i : i + len(needle)] == needle:
             return i
     return -1
-    
