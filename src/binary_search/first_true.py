9e0594864c7d8f1b991789e776d6a4a199c1c519
Olimov Komil <89007924+komilolimov@users.noreply.github.com>
2026-08-12 17:13:33 +0500

    fix: remove trailing blank lines and fix formatting issues

--- src/binary_search/first_true.py
+++ src/binary_search/first_true.py
@@ -1,4 +1,4 @@
-# An array of boolean values is divided into two sections: The left section consists of all false, and the right section consists of all true. Find the First True in a Sorted Boolean Array of the right section, i.e., the index of the first true element. If there is no true element, return -1.
+# An array of boolean values is divided into two sections: The left section consists of all false, and the right section consists of all true. Find the First True in a Sorted Boolean Array of the r[...]
 
 # Input: arr = [false, false, true, true, true]
 
